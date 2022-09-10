9165807c5659daa11dec9186292840e2613387f9
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-10 08:20:28 -0700

    Task 10 fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
  * main - main block
  * Description: Write a program that prints all possible
@@ -18,35 +16,32 @@
  * All your code should be in the main function
  * Return: 0
  */
-
 int main(void)
-
 {
+	int c;
+	int d = 0;
 
-int c;
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
 
-int d = 0;
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-while (d < 10)
-
-{
-
-c = 0;
-										while (c < 10)
-
-										{
-
-										if (d != c && d < c)
-																				{																				putchar('0' + d);
-										putchar('0' + c);																		if (c + d != 17)
-										putchar(',');																			putchar(' ');									}																				}
-										c++;
-										}
-										d++;
-										}
-
-putchar('\n');
-
-return (0);
-
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }

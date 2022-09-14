8b54a96bd60342debe3b76360f8c88a8aed14621
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-14 21:34:17 +0100

    Task 1 fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,19 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-        i = 0;
+i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+while (i < 1)
+{
 
-        printf("Infinite loop avoided! \\o/\n");
+putchar(i);
+}
 
-        return (0);
+printf("Infinite loop avoided! \\%\n");
+
+return (0);
 }

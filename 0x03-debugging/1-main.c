13dad7a27dd2971b7e952f4911ef775b27ec157e
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-15 11:14:57 +0100

    Task 1 fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* REturn: 0
-*/
+* main - cause an infinite loop
+* Return: 0
 
 int main(void)
 {
 int i;
 
-printf("Infinite loop incoming :(\n");
+printf(""Infinite loop incoming :(\n");
+[A[C[C[C[C[C[C[C[[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"Infinite loop incoming :(\n");
 
 i = 0;
 /*
@@ -17,8 +17,12 @@ i = 0;
 *{
 *putchar(i);
 *}
+*/[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C 0
 */
-printf("Infinte loop avoided! \\o/\n");
+[A[C[C
+
+[B[B[B[B[B[B[B[B[B[B[B[C[C
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }

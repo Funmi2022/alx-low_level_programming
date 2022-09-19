0da4afae2804aea24bf7cd6fdb7d609eb853fde8
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-19 15:30:26 +0100

    Function that takes pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,39 @@
+[[C[C[C[C[C[C[D[D[D[[C[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[cat > 0-reset_to_98.c
+#include <stdio.h>
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+#include <main.h"[D[D[D[D[D[D[[C"main.h"
+
+/**
+* main - function that takes a pointer to an int
+*
+*Return: Alaways 0
+*\
+
+int main (void)
+{
+int n;
+n = 204
+printf("n=%d\n", n)
+reser_to_98(&n)
+[A[C[C[C[C[t[B[D[D[D[D[D[D[A[A[A[C[C[C[C[C[C[C;[B[C[C[C[C[C[C[C[C[C[C[C;[B[C[D[D[D[D[D[D;
+printf("n=%d\n", n);
+return (0);
+
+}[A[A[A[A[A[A[A[A                                                                    
+[A[A[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[A[Bint n;
+
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
+
+}
+[A[A[A[A[A[A[B
+ [3~[B[B[[A[A[C[C[C[C[C[[A[A[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[Bn = 392;
+printf("n=%d\n", n);
+reset_to_98(&n);
+print("n=%d\n", n);
+return (0);
+
+}

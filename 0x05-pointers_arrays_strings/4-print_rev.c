29f3b76c091239582867c6f5a7f35a5904404512
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-20 00:07:42 +0100

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,4 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
-
-/**
-*print_rev - Prints a string in reverse order
-*@s: String to reverse
-*Return: Nothing
-*/
-
-void print_rev(char *s)
-{
-	int len = strlen(s);
-
-	while (len--)
-		putchar(*(s + len));
-	putchar(10);
-}[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[3~[3~[3~[D[3~[3~[D[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ci--)[B[C[C[C[C[C[C[C[C[C[C[C[ i = str(s);[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[C
-[A[[A[A[A[B[B[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[C[A[C[C[C[C[C[C[C[C[C[C[[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[[A[A[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C#include "main.h"
 
 /**
  * print_rev - prints string beckwards

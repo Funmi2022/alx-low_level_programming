0a227616f628781a655d87a17fc9d8d76c948d86
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-19 17:31:55 +0100

    Task 0 fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * main - reset_to_98 - resets the value of given integer to 98
@@ -11,10 +10,11 @@ int main (void)
 {
 
 int n;
-n = 98
-printf("%d\n", *n)
-reset_to_98(&n)
-printf("%d\n", *n);
+*n = 98
+printf("%d\n", *p);
+reset_to_98(&n);
+printf("%d\n", *p);
+
 return (0);
 
 }

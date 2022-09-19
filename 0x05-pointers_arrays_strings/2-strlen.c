61d6396a69b15b90999a96e8594bee69b379c8f5
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-19 22:20:43 +0100

    Task 2 fixed

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,8 +10,10 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	return (len);
-}[A[A[A[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[A[C[C[][D[D[D[D[D[D[D[[C[C[s[B[B[B[C[C[C[C[D[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[C
+}

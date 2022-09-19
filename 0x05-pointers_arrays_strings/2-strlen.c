ea8eeb5945750e170e5626fab52ee7a921163cd7
Funmi2022 <Oluwafunmiketosin@gmail.com>
2022-09-19 22:07:24 +0100

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}[A[A[A[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[A[C[C[][D[D[D[D[D[D[D[[C[C[s[B[B[B[C[C[C[C[D[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[C
